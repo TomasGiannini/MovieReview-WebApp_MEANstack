c5ed8fcd80598c3f16de6f5eb20250f1a10d8355
TomasGiannini <tomasjgiannini@gmail.com>
2019-11-19 15:44:05 -0500

    add user functionality added w/ hash password

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const router = express.Router();
 
+// hash password and save user to database
 router.post('/signup', (req, res, next) => {
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password
-  });
+  // hash the password, 10 refers to salting rounds
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(result => {
+          res.status(201).json({
+            message: 'user created',
+            result: result
+          });
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          });
+        });
+    });
 });
 
 
